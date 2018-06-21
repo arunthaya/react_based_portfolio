2d22d83ce10ed7d5277c68a1d905854f9a524847
Arun Thayanithy <arunthaya@gmail.com>
2018-06-21 12:38:12 -0400

    Changed nav css, background
    - Added Typing NPM
    - Changed nav bar css
    - Changed background of page css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Menu, Segment } from 'semantic-ui-react';
+import '../App.css';
 import Nav from './Nav';
 
 const Header = () => (
-    <Menu secondary>
-        <Menu.Item as={Nav} to="/" name="home" />
-        <Menu.Item as={Nav} to="/about" name="messages" />
-        <Menu.Item as={Nav} to="/sample" name="friends" />
-    </Menu>
+    <div className="navMenu">
+        <Segment inverted>
+            <Menu inverted pointing secondary>
+                <Menu.Item as={Nav} to="/" name="about me" className="cool-link"/>
+                <Menu.Item as={Nav} to="/about" name="projects"/>
+                <Menu.Item as={Nav} to="/sample" name="resume"/>
+            </Menu>
+        </Segment>
+    </div>
 );
 
 export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../App.css';
+import Typing from 'react-typing-animation';
 
 class Home extends React.Component{
     constructor(props){
@@ -6,7 +8,11 @@ class Home extends React.Component{
     }
     render(){
         return(
-            <p>Hello</p>
+            <div className="main-content">
+                <Typing>
+                    <span className="animatedTitle"><h1>This span will get typed.</h1></span>
+                </Typing>
+            </div>
         );
     }
 }
