e7c51229dedd6605a8a40940fe63f7ae7096db22
Arun Thayanithy <arunthaya@gmail.com>
2018-06-29 22:15:33 -0400

    Success Modal
    - Addition of success modal
    - Removal of failure modal

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Form, Container, Message } from 'semantic-ui-react';
+import Modal from 'react-responsive-modal';
 
 class Contact extends React.Component {
     state = {
@@ -10,12 +11,20 @@ class Contact extends React.Component {
         emailError: false,
         messageError: false,
         formError: false,
-        formSuccess: false
+        open: false
+    };
+
+    onOpenModal = () => {
+        this.setState({ open: true});
+    };
+
+    onCloseModal = () => {
+        this.setState({ open: false});
     };
 
     handleChange = (e, {name, value}) => {
         console.log(`name coming in is ${name} and the value is: ${value}`);
-        this.setState({ [name]: value, formSuccess: false});
+        this.setState({ [name]: value});
         let variableBeingEntered = name;
         switch(variableBeingEntered){
             case "name":
@@ -57,38 +66,38 @@ class Contact extends React.Component {
                 emailError: false,
                 messageError: false,
                 formError: false,
-                formSuccess: true
             });
+            this.onOpenModal();
         } else {
             this.setState({ formError: true });
         }
     };
 
     render(){
-        const {name, email, message, nameError, emailError, messageError, formError, formSuccess} = this.state;
+        const {name, email, message, nameError, emailError, messageError, formError, formSuccess, open} = this.state;
         return(
-            <Container>
-                <h1 style={{color: 'white'}}>Use this form to contact me: </h1>
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group widths='equal'>
-                        <Form.Input placeholder='Name' name='name' value={name} onChange={this.handleChange} error={nameError}/>
-                        <Form.Input placeholder='Email' name='email' value={email} onChange={this.handleChange} error={emailError}/>
-                    </Form.Group>
-                    <Form.TextArea placeholder='Message' name='message' value={message} onChange={this.handleChange} error={messageError}/>
-                    <Form.Button>Submit</Form.Button>
-                    {formError && (
-                        <Form error>
-                            <Message error header='Form errors' content='Double check all input(s) in red' />
-                        </Form>
-                    )}
-                    {formSuccess && (
-                        <Form success>
-                            <Message success header='Form Submitted!' content='I will get back to you within 48 hours.'/>
-                        </Form>
-                    )}
-                </Form>
-
-            </Container>
+            <div>
+                <Container>
+                    <h1 style={{color: 'white'}}>Use this form to contact me: </h1>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group widths='equal'>
+                            <Form.Input placeholder='Name' name='name' value={name} onChange={this.handleChange} error={nameError}/>
+                            <Form.Input placeholder='Email' name='email' value={email} onChange={this.handleChange} error={emailError}/>
+                        </Form.Group>
+                        <Form.TextArea placeholder='Message' name='message' value={message} onChange={this.handleChange} error={messageError}/>
+                        <Form.Button>Submit</Form.Button>
+                        {formError && (
+                            <Form error>
+                                <Message error header='Form errors' content={`Double check all input(s) in red`}/>
+                            </Form>
+                        )}
+                    </Form>
+                </Container>
+                <Modal open={open} onClose={this.onCloseModal} center>
+                    <h1>Form Successfully Submitted!</h1>
+                    <p>Expect a response in 48 hours. This form was coded by hand :)</p>
+                </Modal>
+            </div>
         );
     }
 }
